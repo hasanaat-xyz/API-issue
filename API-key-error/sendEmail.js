985da41a644cc595a53386dadc188ad4a5dff649
hasanaat <arsheema619@gmail.com>
2025-10-08 19:15:54 +0530

    sendEmail

--- API-key-error/sendEmail.js
+++ API-key-error/sendEmail.js
@@ -1,23 +1,21 @@
 import { Resend } from "resend";
 
-let apiKey = 'apikey';
+let apiKey = "apikey";
 
 const resend = new Resend(apiKey);
 
-async function sendEmail(emailData){
-    const {data, error} = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: emailData.to,
-        subject: emailData.subject,
-        html: emailData.body,
-      });
+async function sendEmail(emailData) {
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: emailData.to,
+    subject: emailData.subject,
+    html: emailData.body,
+  });
 
-
-      if (error) {
-        return console.log({error});
-    }
-    console.log({data});
-
+  if (error) {
+    return console.log({ error });
+  }
+  console.log({ data });
 }
 
 // sendEmail();
